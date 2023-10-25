dfd4737dde893a437ec2f8525194b16de6c04ddc
polinatea <polinachainikova99@gmail.com>
2023-10-25 14:06:17 +0300

    added new work item, fixed about font

--- pages/work.js
+++ pages/work.js
@@ -2,14 +2,24 @@ import React from 'react';
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import Link from 'next/link';
 
 import 'swiper/css';
 const Work = () => {
 
   const projects = [
     {
+      id: 'LovingHut',
+      projectName: 'Loving hut',
+      link: 'https://restaurant-nu-five.vercel.app/',
+      about: `A website for the restaurant Loving Hut with menu filter by category`,
+      technologies: ['Typescript', 'Next.js', 'Tailwind'],
+      projectPhoto: ['loving-hut', 'loving-hut2', 'loving-hut3']
+    },
+    {
       id: 'KindCompass',
       projectName: 'Volunteers app',
+      link: 'https://github.com/polinatea/volunteers-app',
       about: `A social media that helps users
       find volunteer events depending on their skills. Users can view events in a list and map
       modes, respond to events, create events, check responses to their events.`,
@@ -19,6 +29,7 @@ const Work = () => {
     {
       id: 'Restaurant',
       projectName: 'Restaurant website',
+      link: 'https://polinatea.github.io/simple-restaurant-website/#',
       about:  `A simple responsive restaurant website. My first project since I had started learning frontend development`,
       technologies: ['HTML', 'CSS', 'Javascript'],
       projectPhoto: ['restaurant-website', 'restaurant-website2']
@@ -31,7 +42,7 @@ const Work = () => {
       // <div key={project.id} className='h-[calc(100vh-80px)] flex flex-col items-center lg:flex-row lg:justify-center  gap-10 '>
       <div key={project.id} className=' flex flex-col items-center lg:flex-row lg:justify-center  gap-10 sm:pt-20'>
         <div className='w-[360px]  xl:w-[540px] flex flex-col gap-5  text-gray-700 px-5  '>
-          <p className='lg:text-2xl text-xl font-medium text-center'>{project.projectName}</p>
+          <Link href={project.link}><p className='lg:text-2xl text-xl font-medium text-center underline underline-offset-4'>{project.projectName}</p></Link>
           <p className='lg:text-xl text-sm font-normal'>{project.about}</p>
          <div className='lg:text-base text-xs font-normal flex gap-2 flex-wrap'> 
          {project.technologies.map((technology)=>(
